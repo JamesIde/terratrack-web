d314707550d4cac997e7c94ea08a5def085625ca
James <james.ide775@gmail.com>
2023-06-12 09:18:55 +0930

    Setup mapbox

---
+++ config/config.ts
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  MAPBOX: {
+    ACCESS_TOKEN:
+      "pk.eyJ1IjoiMW1hY3JvcyIsImEiOiJjbGYzZndjb3QwZzZzM3NwZnNheGhpcGEyIn0.1kFMlajw3UonchFxVMZ-8A",
+  },
+};
---
+++ src/app/sidebar.tsx
@@ -0,0 +1,8 @@
+function Sidebar() {
+  return (
+    <aside className="w-[500px] h-screen bg-blue-500">
+      <p className="font-bold text-red-500">sidebar</p>
+    </aside>
+  );
+}
+export default Sidebar;
