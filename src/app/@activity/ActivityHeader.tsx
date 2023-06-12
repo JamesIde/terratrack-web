b8c6e34798e61b61303e44caa781f0dbedd3958b
James <james.ide775@gmail.com>
2023-06-12 13:31:01 +0930

    Header

---
+++ src/app/@activity/ActivityHeader.tsx
@@ -0,0 +1,20 @@
+function ActivityHeader() {
+  return (
+    <div className="flex flex-row justify-between">
+      <h1 className="font-bold text-2xl">All Activities</h1>
+      <div className="w-24">
+        <select
+          id="countries"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option selected>Sort By</option>
+          <option value="US">United States</option>
+          <option value="CA">Canada</option>
+          <option value="FR">France</option>
+          <option value="DE">Germany</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+export default ActivityHeader;
